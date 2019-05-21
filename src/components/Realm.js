94049c4b681233b2e7d6a39cdbd1577943a039ee
Scott Weidenkopf <sweidenkopf@gmail.com>
2019-05-20 22:13:36 -0400

    added reactive mouse movement

--- src/components/Realm.js
+++ src/components/Realm.js
@@ -14,22 +14,39 @@ export default class Realm extends React.Component {
         this.app = new PIXI.Application({transparent: true});
         this.gameCanvas.appendChild(this.app.view);
         this.grid.forEach((hex) => {
-            const graphics = new PIXI.Graphics()
-            graphics.lineStyle(1, 0x999999)
-            const point = hex.toPoint()
+            const graphics = new PIXI.Graphics();
+            graphics.lineStyle(1, 0x000000);
+            graphics.beginFill(0x999999);
+            const point = hex.toPoint();
             // add the hex's position to each of its corner points
-            const corners = hex.corners().map(corner => corner.add(point))
+            const corners = hex.corners().map(corner => corner.add(point));
             // separate the first from the other corners
-            const [firstCorner, ...otherCorners] = corners
+            const [firstCorner, ...otherCorners] = corners;
 
             // move the "pen" to the first corner
-            graphics.moveTo(firstCorner.x, firstCorner.y)
+            graphics.moveTo(firstCorner.x, firstCorner.y);
             // draw lines to the other corners
-            otherCorners.forEach(({ x, y }) => graphics.lineTo(x, y))
+            otherCorners.forEach(({ x, y }) => graphics.lineTo(x, y));
             // finish at the first corner
-            graphics.lineTo(firstCorner.x, firstCorner.y)
+            graphics.lineTo(firstCorner.x, firstCorner.y);
 
-            this.app.stage.addChild(graphics)
+            graphics.interactive = true;
+
+            const points = corners.map((corner) => new PIXI.Point(corner.x, corner.y));
+
+            graphics.hitArea = new PIXI.Polygon(points);
+
+            // make graphics non-transparent when mouse is over it
+            graphics.mouseover = function(mouseData) {
+                this.alpha = 0.5;
+            }
+            
+            // make graphics half-transparent when mouse leaves
+            graphics.mouseout = function(mouseData) {
+                this.alpha = 1;
+            }
+
+            this.app.stage.addChild(graphics);
         });
         this.app.start();
     }
