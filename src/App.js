5945fa7a8cbd5fc0c21e6ce0abc20ab8df494c6b
Scott Weidenkopf <sweidenkopf@gmail.com>
2019-05-25 21:13:56 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { HashRouter, Route } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+
+import MainMenu from './components/MainMenu';
 import RealmContainer from './containers/RealmContainer';
+
+import Play from './routes/Play';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RealmContainer/>
+        <HashRouter>
+          <Route exact path="/" component={MainMenu}/>
+          <Route exact path="/play/" component={RealmContainer}/>
+        </HashRouter>
       </div>
     );
   }
---
+++ src/components/MainMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function MainMenu() {
+  return (
+      <div>
+        <Link to="/play">Play</Link>
+      </div>
+  );
+}
+
+export default MainMenu;
